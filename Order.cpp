0e3e206d07822cc1f452a9c432d3f68a23a94ccf
AwesomeNipun <nipun1969@gmail.com>
2023-12-15 15:11:29 +0530

    Modify order class

--- Order.cpp
+++ Order.cpp
@@ -3,3 +3,103 @@
 //
 
 #include "Order.h"
+
+// constructor
+Order::Order(const string& ClientOrderID, const string& Instrument, int Side, int Quantity, double Price) {
+
+    clientOrderID = ClientOrderID;
+    instrument = Instrument;
+    side = Side;
+    quantity = Quantity;
+    price = Price;
+}
+
+// setters and getters
+
+const string& Order::getClientOrderID() const {
+    return clientOrderID;
+}
+
+void Order::setClientOrderID(const string& ClientOrderID) {
+    clientOrderID = ClientOrderID;
+}
+
+const string& Order::getInstrument() const {
+    return instrument;
+}
+
+void Order::setInstrument(const string& Instrument) {
+    instrument = Instrument;
+}
+
+int Order::getSide() const {
+    return side;
+}
+
+void Order::setSide(int Side) {
+    side = Side;
+}
+
+int Order::getQuantity() const {
+    return quantity;
+}
+
+void Order::setQuantity(int Quantity) {
+    quantity = Quantity;
+}
+
+double Order::getPrice() const {
+    return price;
+}
+
+void Order::setPrice(double Price) {
+    price = Price;
+}
+
+const string& Order::getStatus() const {
+    return status;
+}
+
+void Order::setStatus(const string& Status) {
+    status = Status;
+}
+
+const string& Order::getTransactTime() const {
+    return transactTime;
+}
+
+void Order::setTransactTime(const string& TransactTime) {
+    transactTime = TransactTime;
+}
+
+const string& Order::getReason() const {
+    return reason;
+}
+
+void Order::setReason(const string& Reason) {
+    reason = Reason;
+}
+
+const string& Order::getOrderID() const {
+    return orderID;
+}
+
+void Order::setOrderID(const string& OrderID) {
+    orderID = OrderID;
+}
+
+const string& Order::getExecStatus() const {
+    return execStatus;
+}
+
+void Order::setExecStatus(const string& ExecStatus) {
+    execStatus = ExecStatus;
+}
+
+
+
+
+
+
+
+
--- Order.h
+++ Order.h
@@ -5,10 +5,48 @@
 #ifndef FLOWER_EXCHANGE_ORDER_H
 #define FLOWER_EXCHANGE_ORDER_H
 
+#include <iostream>
+using namespace std;
+#include <string>
 
 class Order {
+private:
 
+    string clientOrderID;
+    string instrument;
+    int side;
+    int quantity;
+    double price;
+    string status;
+    string transactTime;
+    string reason;
+    string execStatus;
+    string orderID;
+
+public:
+    Order(const string &ClientOrderID, const string &Instrument, int Side, int Quantity, double Price);
+
+    const string &getClientOrderID() const;
+    const string &getInstrument() const;
+    int getSide() const;
+    int getQuantity() const;
+    double getPrice() const;
+    const string &getStatus() const;
+    const string &getTransactTime() const;
+    const string &getReason() const;
+    const string &getOrderID() const;
+    const string &getExecStatus() const;
+
+    void setClientOrderID(const string &ClientOrderID);
+    void setInstrument(const string &Instrument);
+    void setSide(int Side);
+    void setQuantity(int Quantity);
+    void setPrice(double Price);
+    void setStatus(const string &Status);
+    void setTransactTime(const string &TransactTime);
+    void setReason(const string &Reason);
+    void setOrderID(const string &OrderID);
+    void setExecStatus(const string &ExecStatus);
 };
-
 
 #endif //FLOWER_EXCHANGE_ORDER_H
